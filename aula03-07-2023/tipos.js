5acb3ebae469a2ac9a4d745ac2f4f28fc90f692d
quilantera <gugaquilante@gmail.com>
2023-07-03 13:55:01 -0300

    continuaçao aula03-07-2023

---
+++ aula03-07-2023/tipos.js
@@ -0,0 +1,33 @@
+//tipo dinamica -> O Tipo da variavel é definido de acordo com o seu valor
+
+//typeof -> operador que retorna o tipo da variavel;
+let variavel= 22;
+
+console.log(typeof variavel);
+
+variavel="Gustavo";
+
+console.log(typeof variavel);
+
+variavel= true;
+
+console.log(typeof variavel);
+
+let objeto = {nome: "Gustavo", idade: 23};
+console.log(variavel);
+
+let vetor = [0,1,2,3,4];
+console.log(vetor);
+
+function funcao(){
+   return(1);
+};
+
+var indefinido = undefined;
+console.log(indefinido);
+
+var nulo = null;
+console.log(nulo);
+
+var notANumber = NaN; 
+console.log(notANumber);
---
+++ aula04-07-2023/operadores.js
@@ -0,0 +1,30 @@
+const num1 = 5;
+const num2 = 8;
+const num3 = 4.5;
+
+/*
+  Operadores Aritméticos
+    + Adição
+    -Subtração
+    * Multiplicação
+    / Divisão
+    ** Exponenciação
+    % Resto da divisão ( modulo ))
+*/
+
+let result = num1 + num2; //13
+console.log(result);
+
+result = num1 - num2;
+console.log(result); // -3
+
+result = num1 * num2; // 40
+console.log(result);
+
+result = num1 / num2; // 0.625
+console.log(result);
+
+result = num1%num2; 
+console.log(result);
+
+result = num1**num2;
