aca16a0c429a2e28156a58475b8df6f9c1edfa95
quilantera <gugaquilante@gmail.com>
2023-07-05 14:22:48 -0300

    aula-05-07-2023

--- aula05-07-2023/estruturas-condicionais.js
+++ aula05-07-2023/estruturas-condicionais.js
@@ -63,3 +63,44 @@ if( result1){
     console.log(num2+">"+num3+">"+num1);
   }else console.log(num3+">"+num2+">"+num1);
 }
+
+//switch case:
+let mes = 10
+switch (mes){
+    case 1:
+    console.log('janeiro');
+    break;
+  case 2:
+    console.log('fevereiro');
+    break;
+  case 3:
+    console.log('março');
+    break;
+  case 4:
+    console.log('abril');
+    break;
+  case 5:
+    console.log('maio');
+    break;
+  case 6:
+    console.log('junho');
+    break;
+  case 7:
+    console.log('julho');
+    break;
+  case 8:
+    console.log('agosto');
+    break;
+  case 9:
+    console.log('setembro');
+    break;
+  case 10:
+    console.log('outubro');
+    break;
+  case 11:
+    console.log('novembro');
+    break;
+  case 12:
+    console.log('dezembro');
+    break;
+}
